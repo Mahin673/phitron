b81f266bae410e5c156645b474ea4a2382f0d1c3
Mahin673 <rakibulhasanmahin673@gmail.com>
2026-09-06 00:33:35 +0600

    Add files via upload

---
+++ C++ Data Structure/String_constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  // string s = "Hello";
+  // 1
+  // string s("Hello");
+  // 2
+  // string s("Hello World", 4); // here 4 is output and 4 er por sob delete
+  // 3
+  // string s = "Hello World";
+  // 4
+  // string s(10, 'B');//ptint 10ta B
+  cout << s << endl;
+
+  return 0;
+}
